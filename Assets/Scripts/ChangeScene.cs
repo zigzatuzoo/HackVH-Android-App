971f6d47b7dd35ab904a8aec15b254a733d16756
Nking1247 <236442@glenbrook225.org>
2021-04-17 13:09:52 -0500

    haa

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Events;
 
 
 public class ChangeScene : MonoBehaviour

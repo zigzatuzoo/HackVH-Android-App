8df51b8b1036e022f15593d14d8c4736cf3eae00
Unknown <33558297+zigzatuzoo@users.noreply.github.com>
2021-04-18 07:14:38 -0700

    Timer update

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,18 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float timeRemaining;
     public bool timerIsRunning = false;
     public Text timeText;
+    public Text currentstattext;
+    public Button finishButton;
+    public Button cancelButton;
 
-    private void Start()
+    private int cyclesLeft;
+
+    public int breakLengthInt;
+    public int numBreaksInt;
+    public int studyTimeInt;
+    public float mathFloat;
+
+    private void Awake()
     {
-        // Starts the timer automatically
+        breakLengthInt = PlayerPrefs.GetInt("breakLengthPref");
+        numBreaksInt = PlayerPrefs.GetInt("numBreakPref");
+        studyTimeInt = PlayerPrefs.GetInt("studyTimePref");
+        mathFloat = PlayerPrefs.GetFloat("mathPref");
+
+        Debug.Log("breakLengthInt " + breakLengthInt);
+        Debug.Log("numBreaksInt " + numBreaksInt);
+        Debug.Log("studyTimeInt " + studyTimeInt);
+        Debug.Log("mathFloat" + mathFloat);
+
         timerIsRunning = true;
-        timeRemaining = 60;
+        timeRemaining = mathFloat;
+
+        cyclesLeft = numBreaksInt * 2;
+        currentstattext.text = "Study!";
+
+        Button fnbtn = finishButton.GetComponent<Button>();
+        fnbtn.onClick.AddListener(toMainMenu);
+
+        Button cnbtn = cancelButton.GetComponent<Button>();
+        cnbtn.onClick.AddListener(toTimerMath);
     }
 
     void Update()
@@ -30,7 +59,41 @@ public class Timer : MonoBehaviour
                 Debug.Log("Time has run out!");
                 timeRemaining = 0;
                 timerIsRunning = false;
+                Switch2Stuff();
             }
+        }
+    }
+
+    void Switch2Stuff()
+    {
+        cyclesLeft = cyclesLeft - 1;
+        Debug.Log("onSwitch2stuff, current cyclesLeft:" + cyclesLeft);
+
+        if(cyclesLeft == 0)
+        {
+            currentstattext.text = "Done!";
+            finishButton.interactable = true;
+            Debug.Log("done");
+        }
+
+        if(cyclesLeft % 2 == 0)
+        {
+            // even number so its a break
+            timeRemaining = breakLengthInt * 60;
+            timerIsRunning = true;
+            Debug.Log("even session");
+        }
+
+        if(cyclesLeft % 2 == 1)
+        {
+            // odd number so its a study session
+            timeRemaining = mathFloat * 60;
+            timerIsRunning = true;
+            Debug.Log("odd session");
+        }
+        else
+        {
+            Debug.LogError("something happened during Switch2Stuff");
         }
     }
 
@@ -40,8 +103,16 @@ public class Timer : MonoBehaviour
 
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        float milliSeconds = (timeToDisplay % 1) * 1000;
 
-        timeText.text = string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds,milliSeconds);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void toMainMenu()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+    void toTimerMath()
+    {
+        SceneManager.LoadScene("TimerMath");
     }
 }

b5bdf9ac3064d6092762505f2d420253064ec739
Nking1247 <236442@glenbrook225.org>
2021-04-18 00:25:29 -0500

    okiedokie

---
+++ Assets/Scripts/Toggles.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toggles : TimerMath
+{
+    // Start is called before the first frame update
+    
+    
+    public Toggle inGameHourToggle;
+    public Toggle inGameMinuteToggle;
+  
+    
+
+    private void Start()
+     {
+        
+
+        //Debug.Log("value = " + inGameHourToggle.isOn);
+
+     }
+
+     //Use buttons linked to this
+     public void ChangeValueToTrue(Toggle aToggle)
+     {
+         aToggle.GetComponent<Toggle>().isOn = true;
+     }
+ 
+     //Use buttons linked to this
+     public void ChangeValueToFalse(Toggle aToggle)
+     {
+         aToggle.GetComponent<Toggle>().isOn = false;
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        whatToggleOn();
+        
+    }
+
+    
+    void whatToggleOn()
+    {
+        if (inGameMinuteToggle.isOn)
+        {
+            ChangeValueToFalse(inGameHourToggle);
+            //inGameHourToggle.isOn = false;
+            
+
+        }
+        if (inGameHourToggle.isOn)
+        {
+            ChangeValueToFalse(inGameMinuteToggle);
+            //inGameMinuteToggle.isOn = false;
+            
+            
+        }
+    }
+    
+    
+    public override void submit()
+    {
+        if(inGameMinuteToggle.isOn == false && inGameHourToggle.isOn = false)
+        {
+            int intBreakLength;
+            int intNumBreaks;
+            int intStudyTime;
+            intBreakLength = int.Parse(breakLength.text);
+            intNumBreaks = int.Parse(numOfBreaks.value.ToString());
+            intStudyTime = int.Parse(studyTime.text);
+            Debug.Log(intStudyTime);
+            Debug.Log(intBreakLength);
+
+            double math = (intStudyTime / (intNumBreaks + 1)) - (intBreakLength / 2);
+            Debug.Log(math);
+
+            List<double> test = new List<double>();
+            string display;
+
+            int rep = 0;
+            while (rep != intNumBreaks)
+            {
+                test.Add(math);
+                test.Add(intBreakLength);
+                rep = rep + 1;
+            }
+            test.Add(math);
+
+            display = "";
+
+            foreach (var num in test)
+            {
+                Debug.Log(num.ToString());
+                display = display.ToString() + num.ToString() + " Minutes" + "\n";
+            }
+            output.text = display;
+            time = display;
+
+            startButton.interactable = true;
+            Button btn = startButton;
+            btn.onClick.AddListener(toTimer);
+        }
+        else if (inGameHourToggle.isOn)
+        {
+            int intBreakLength;
+            int intNumBreaks;
+            int intStudyTime;
+            intBreakLength = int.Parse(breakLength.text);
+            intNumBreaks = int.Parse(numOfBreaks.value.ToString());
+            intStudyTime = int.Parse(studyTime.text);
+            Debug.Log(intStudyTime);
+            Debug.Log(intBreakLength);
+        }
+        else if (inGameMinuteToggle.isOn)
+        {
+            int intBreakLength;
+            int intNumBreaks;
+            int intStudyTime;
+            intBreakLength = int.Parse(breakLength.text);
+            intNumBreaks = int.Parse(numOfBreaks.value.ToString());
+            intStudyTime = int.Parse(studyTime.text);
+            Debug.Log(intStudyTime);
+            Debug.Log(intBreakLength);
+        }
+        
+    }
+
+}

82e43f7102be94d64129a7c2df00d731809f4068
nathanferrell <46502922+nathanferrell@users.noreply.github.com>
2021-04-17 20:01:27 -0400

    Update RandomURL.cs

--- Assets/Scripts/RandomURL.cs
+++ Assets/Scripts/RandomURL.cs
@@ -21,8 +21,19 @@ public class RandomURL : MonoBehaviour
     {
         var urlList = new ArrayList()
         {
-            "https://youtube.com",
-            "https://google.com"
+            "https://www.mindful.org/how-to-meditate-3/",
+            "https://www.gaiam.com/blogs/discover/meditation-101-techniques-benefits-and-a-beginner-s-how-to",
+            "https://www.mentalhealth.gov/get-help/immediate-help",
+            "https://inspirobot.me",
+            "https://sunshineadc.com/7-fun-games-improve-mental-health/",
+            "https://www.verywellmind.com/top-websites-and-games-for-brain-exercise-2224140",
+            "https://greatergood.berkeley.edu/article/item/ten_ways_to_become_more_grateful1",
+            "https://careersinpsychology.org/7-quick-easy-mental-health-exercises-help-day/",
+            "https://www.acmc.edu/how-students-can-stay-positive-towards-school/",
+            "https://www.caba.org.uk/help-and-guides/information/how-and-why-boost-your-alpha-brainwaves"
+            
+            
+            
         };
 
         int randNum = Random.Range(0, urlList.Count);

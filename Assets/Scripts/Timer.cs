144d32dae9eb8f0dddfa9cffba2d882c99098ab7
Unknown <33558297+zigzatuzoo@users.noreply.github.com>
2021-04-17 19:43:22 -0700

    Timer update

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,39 +5,43 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public float timeRemaining;
+    public bool timerIsRunning = false;
+    public Text timeText;
 
-    public InputField studyStart;
-    public InputField studyEnd;
-    public Button submitButton;
-
-    
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Button subbtn = submitButton.GetComponent<Button>();
-        subbtn.onClick.AddListener(submit);
+        // Starts the timer automatically
+        timerIsRunning = true;
+        timeRemaining = 60;
     }
 
-    void submit()
-    {
-        Debug.Log("submit button clicked");
-        Debug.Log(studyStart.text);
-        Debug.Log(studyEnd.text);
-
-        int intStudyStart;
-        int intStudyEnd;
-        intStudyStart = int.Parse(studyStart.text);
-        intStudyEnd = int.Parse(studyEnd.text);
-
-        int totalTime = intStudyEnd - intStudyStart;
-
-        Debug.Log(totalTime);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = (timeToDisplay % 1) * 1000;
+
+        timeText.text = string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds,milliSeconds);
     }
 }
